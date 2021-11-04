00a6d72330679ec05de38944a68790980f0bfbe3
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-04 01:46:53 -0300

    Update GenreController.java

--- disney-api/src/main/java/com/alkemy/disneyapi/controller/GenreController.java
+++ disney-api/src/main/java/com/alkemy/disneyapi/controller/GenreController.java
@@ -1,4 +1,10 @@
 package com.alkemy.disneyapi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("continentes")
 public class GenreController {
+
 }
