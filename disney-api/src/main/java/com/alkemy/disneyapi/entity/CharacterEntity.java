9a67a86c8b6cc6b96e510f5986967a12ccd1d473
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-03 23:17:57 -0300

    CharacterEntity DONE

--- disney-api/src/main/java/com/alkemy/disneyapi/entity/CharacterEntity.java
+++ disney-api/src/main/java/com/alkemy/disneyapi/entity/CharacterEntity.java
@@ -1,15 +1,48 @@
 package com.alkemy.disneyapi.entity;
 
 
+import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "character")
 @Getter
 @Setter
+@SQLDelete(sql = "UPDATE character SET deleted = true WHERE id=? ")
+@Where(clause = "deleted=false")
 public class CharacterEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String image;
+
+    private String name;
+
+    private Integer age;
+
+    private Double weight;
+
+    private String history;
+
+    private boolean deleted = Boolean.FALSE;
+
+    @ManyToMany(mappedBy = "characters", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<MovieEntity> movies = new ArrayList<>();
+
 }
