dd6c8e137bea117d139116697ec2295ada9da4a9
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-03 23:10:58 -0300

    MovieEntity DONE

--- disney-api/src/main/java/com/alkemy/disneyapi/entity/MovieEntity.java
+++ disney-api/src/main/java/com/alkemy/disneyapi/entity/MovieEntity.java
@@ -2,22 +2,77 @@ package com.alkemy.disneyapi.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "movie")
 @Getter
 @Setter
+@SQLDelete(sql = "UPDATE movie SET deleted = true WHERE id=?")
+@Where(clause = "deleted=false")
 public class MovieEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
+    private String image;
+
+    private String title;
+
+    @Column(name = "creation_date")
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate creationDate;
+
+    private Integer rating; //(1 to 5)
+
+    private boolean deleted = Boolean.FALSE;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST,
+            CascadeType.MERGE,})
+    @JoinColumn(name = "genre_id", insertable = false, updatable = false)
+    private GenreEntity genre;
+
+    @Column(name = "genre_id", nullable = false)
+    private Long genreId;
+
+    @ManyToMany(
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            }
+    )
+    @JoinTable(
+            name = "character_movie",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "character_id")
+    )
+    private Set<CharacterEntity> characters = new HashSet<>();
+
+    public void addCharacterToMovie(CharacterEntity character) {
+        characters.add(character);
+    }
+    public void removeCharacterFromMovie(CharacterEntity character) {
+        characters.remove(character);
+    }
 
 }
