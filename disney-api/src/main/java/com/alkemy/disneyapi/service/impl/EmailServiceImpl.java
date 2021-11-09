d5c8581b420a0742c64a822a4aef8b4aad76602c
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-09 13:50:01 -0300

    SpringSecurity enabled in pom

--- disney-api/src/main/java/com/alkemy/disneyapi/service/impl/EmailServiceImpl.java
+++ disney-api/src/main/java/com/alkemy/disneyapi/service/impl/EmailServiceImpl.java
@@ -1,4 +1,19 @@
 package com.alkemy.disneyapi.service.impl;
 
-public class EmailServiceImpl {
+import com.alkemy.disneyapi.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+
+public class EmailServiceImpl implements EmailService {
+
+    @Autowired
+    private Environment env;
+
+    @Value("${alkemy.icons.email.sender}")
+    private String emailSender;
+
+    public void sendWelcomeEmailTo(String to) {
+
+    }
 }
